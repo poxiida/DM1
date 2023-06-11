46b3e2f7f24b7840be0611a7999d1a17cd991ab4
poxiida <136040611+poxiida@users.noreply.github.com>
2023-06-11 19:37:45 +0900

    Won/lost message modified

--- DiceGame.c
+++ DiceGame.c
@@ -22,8 +22,8 @@ int main()
   printf("Dice 1: %d\n", d1);
   printf("Dice 2: %d\n", d2);
   printf("Total value: %d\n", sum);
-  if(sum > 7) printf("You won!\n");
-  else printf("You lost\n");
+  if(sum > 7) printf("%s won!\n", name);
+  else printf("%s lost\n", name);
   
   return 0;
 }
